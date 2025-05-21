617f806bb66ea4ee194c292fcc25e78324109b7c
letthem <suyeon3662@naver.com>
2025-05-21 17:26:37 +0900

    [LCR-176] feat: api 명세대로 데이터명 변경

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -2,127 +2,13 @@ import { FlatList } from 'react-native-gesture-handler';
 import { S } from './HomeScreen.style';
 import { Dimensions, Image, Text, View } from 'react-native';
 import Swiper from 'react-native-swiper';
+import { bannerItems, hotItems, popUpItems } from '@/constants/Home';
+import { formatDateRange } from '@/utils/FormatDate';
 
 const Images = {
-  popUp01: require('@/assets/images/home/popUp01.webp'),
-  popUp02: require('@/assets/images/home/popUp02.webp'),
-  popUp03: require('@/assets/images/home/popUp03.webp'),
   calendarGray: require('@/assets/images/common/calendar-gray.webp'),
   locationGray: require('@/assets/images/common/location-gray.webp'),
 };
-
-const bannerItems = [
-  {
-    image: Images.popUp01,
-    title: 'THE HOT HOUSE',
-    subtitle: 'LE SSERAFIM 2025 S/S POP-UP',
-  },
-  {
-    image: Images.popUp02,
-    title: 'Season of Memories',
-    subtitle: 'GFRIEND SPECIAL ALBUM POP-UP',
-  },
-  {
-    image: Images.popUp03,
-    title: 'BORN PINK',
-    subtitle: 'BLACKPINK POP-UP EXPERIENCE',
-  },
-  {
-    image: Images.popUp01,
-    title: 'DREAMSCAPE',
-    subtitle: 'NCT DREAM x LINE FRIENDS',
-  },
-];
-
-const hotItems = [
-  {
-    id: '1',
-    title: '여자친구 Season of Memories 여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더현대서울',
-    image: Images.popUp01,
-  },
-  {
-    id: '2',
-    title: '여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더현대서울',
-    image: Images.popUp02,
-  },
-  {
-    id: '3',
-    title: '여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더현대서울',
-    image: Images.popUp03,
-  },
-  {
-    id: '4',
-    title: '여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더현대서울',
-    image: Images.popUp01,
-  },
-];
-
-const popUpItems = [
-  {
-    id: '1',
-    title: '여자친구 Season of Memories 여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더',
-    image: Images.popUp01,
-  },
-  {
-    id: '2',
-    title: '여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더현대서울',
-    image: Images.popUp02,
-  },
-  {
-    id: '3',
-    title: '여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더현대서울',
-    image: Images.popUp03,
-  },
-  {
-    id: '4',
-    title: '여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더현대서울',
-    image: Images.popUp01,
-  },
-  {
-    id: '5',
-    title: '여자친구 Season of Memories 여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더현대서울',
-    image: Images.popUp02,
-  },
-  {
-    id: '6',
-    title: '여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더현대서울',
-    image: Images.popUp03,
-  },
-  {
-    id: '7',
-    title: '여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더현대서울',
-    image: Images.popUp01,
-  },
-  {
-    id: '8',
-    title: '여자친구 Season of Memories',
-    date: '24.11.15 - 24.11.29',
-    location: '서울 영등포구 여의대로 108 더현대서울',
-    image: Images.popUp02,
-  },
-];
 
 export default function HomeScreen() {
   const screenWidth = Dimensions.get('window').width;
@@ -217,18 +103,18 @@ export default function HomeScreen() {
       <S.SectionTitle>WHAT’S HOT</S.SectionTitle>
       <FlatList
         data={hotItems}
-        keyExtractor={item => item.id}
+        keyExtractor={item => item.popupId}
         horizontal
         showsHorizontalScrollIndicator={false}
         renderItem={({ item, index }) => (
           <S.HotCardContainer isFirst={index === 0}>
             <Image
-              source={item.image}
+              source={item.imageUrl}
               style={{ width: 228, height: undefined, aspectRatio: 3 / 4 }}
             />
             <S.Overlay />
             <S.HotCardTextContainer>
-              <S.HotCardTitle>{item.title}</S.HotCardTitle>
+              <S.HotCardTitle>{item.popupName}</S.HotCardTitle>
             </S.HotCardTextContainer>
           </S.HotCardContainer>
         )}
@@ -246,9 +132,9 @@ export default function HomeScreen() {
             }}
           >
             {row.map(item => (
-              <View key={item.id} style={{ width: cardWidth }}>
+              <View key={item.popupId} style={{ width: cardWidth }}>
                 <Image
-                  source={item.image}
+                  source={item.imageUrl}
                   style={{
                     width: cardWidth,
                     height: cardWidth * (4 / 3),
@@ -257,15 +143,17 @@ export default function HomeScreen() {
                   resizeMode="cover"
                 />
                 <View style={{ width: cardWidth, overflow: 'hidden', marginTop: 10 }}>
-                  <S.PopUpCardTitle numberOfLines={1}>{item.title}</S.PopUpCardTitle>
+                  <S.PopUpCardTitle numberOfLines={1}>{item.popupName}</S.PopUpCardTitle>
                   <S.PopUpCardSubTextContainer>
-                    <Image source={Images.calendarGray} style={{ width: 13, height: 13 }} />
-                    <S.PopUpCardSubText>{item.date}</S.PopUpCardSubText>
+                    <Image source={Images.calendarGray} style={{ width: 15, height: 15 }} />
+                    <S.PopUpCardSubText>
+                      {formatDateRange(item.popupOpenDate, item.popupCloseDate)}
+                    </S.PopUpCardSubText>
                   </S.PopUpCardSubTextContainer>
                   <S.PopUpCardSubTextContainer>
-                    <Image source={Images.locationGray} style={{ width: 13, height: 13 }} />
+                    <Image source={Images.locationGray} style={{ width: 15, height: 15 }} />
                     <S.PopUpCardSubText numberOfLines={1} ellipsizeMode="tail">
-                      {item.location}
+                      {item.address}
                     </S.PopUpCardSubText>
                   </S.PopUpCardSubTextContainer>
                 </View>
