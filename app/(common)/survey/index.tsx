d4414066a96065026788e8da10903efc47828b13
yeongseoRyou <95837534+Y0ungse@users.noreply.github.com>
2025-05-23 10:26:03 +0900

    [LCR-189] rebase

--- app/(common)/survey/index.tsx
+++ app/(common)/survey/index.tsx
@@ -1,13 +1,10 @@
 // SurveyQuestionPage.tsx
 import React, { useState } from 'react';
-import { Text, TouchableOpacity, ViewStyle, TextStyle } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { useTheme } from 'styled-components/native';
 import * as S from './SurveyQuestion.style';
 import CustomGradientBtn from '@/components/customGradientBtn/CustomGradientBtn';
 import CustomGrayBtn from '@/components/customGrayBtn/CustomGrayBtn';
 
-const OPTIONS = ['의류', '문구', '포토카드', '인형'];
+const OPTIONS = ['의류', '문구', '포토카드', '인형', '기타'];
 const QUESTIONS = [
   '어떤 종류의 굿즈를\n가장 선호하시나요?',
   '두 번째 질문 텍스트를\n여기에 넣어주세요',
@@ -17,7 +14,6 @@ const QUESTIONS = [
 const TOTAL = QUESTIONS.length;
 
 const SurveyQuestionPage: React.FC = () => {
-  const theme = useTheme();
   const [step, setStep] = useState(1);
   const [selected, setSelected] = useState<string | null>(null);
   const progressPercent = `${((step - 1) / (TOTAL - 1)) * 100}%`;
@@ -43,35 +39,33 @@ const SurveyQuestionPage: React.FC = () => {
             <S.OptionText isSelected={selected === opt}>{opt}</S.OptionText>
           </S.OptionButton>
         ))}
-      </S.Card>
 
-      <S.BottomActions>
-        <CustomGrayBtn
-          title="이전"
-          onPress={() => {
-            setStep(prev => Math.max(1, prev - 1));
-            setSelected(null);
-          }}
-          disabled={step === 1}
-          style={{ flex: 1, marginRight: 8 }}
-          fontSize={14}
-          height={51} // 공통 컴포넌트 height prop
-        />
-
-        <CustomGradientBtn
-          title="다음"
-          onPress={() => {
-            if (step < TOTAL) {
-              setStep(prev => prev + 1);
+        <S.BottomActions>
+          <CustomGrayBtn
+            title="이전"
+            onPress={() => {
+              setStep(prev => Math.max(1, prev - 1));
               setSelected(null);
-            }
-          }}
-          disabled={!selected}
-          style={{ flex: 1, marginLeft: 8 }}
-          fontSize={18}
-          height={51}
-        />
-      </S.BottomActions>
+            }}
+            disabled={step === 1}
+            style={{ width: '50%' }}
+            fontSize={18}
+          />
+
+          <CustomGradientBtn
+            title="다음"
+            onPress={() => {
+              if (step < TOTAL) {
+                setStep(prev => prev + 1);
+                setSelected(null);
+              }
+            }}
+            disabled={!selected}
+            style={{ flex: 1 }}
+            fontSize={18}
+          />
+        </S.BottomActions>
+      </S.Card>
     </S.Container>
   );
 };
