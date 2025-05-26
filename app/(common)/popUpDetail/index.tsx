853e05099104d6e8dec7c204498c5c9818c6bbd5
yeongseoRyou <95837534+Y0ungse@users.noreply.github.com>
2025-05-26 14:50:52 +0900

    [LCR-212] rebase

--- app/(common)/popUpDetail/index.tsx
+++ app/(common)/popUpDetail/index.tsx
@@ -1,100 +1,102 @@
-import { ScrollView, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native';
 import CustomGradientBtn from '@/components/customGradientBtn/CustomGradientBtn';
 import { S } from './PopUpDetail.style';
+import { MockItems } from '@/mocks/PopUpDetailItemMocks';
+import { PopUpDetailItem } from '@/types/DetailScreenItem';
+import { useCallback } from 'react';
 import { PopUpDetailMock } from '@/mocks/PopUpDetailMocks';
-import { useRouter } from 'expo-router';
-import { ParseJsonToString } from '@/utils/JsonParser';
-import HotItems from '@/components/entireItems/hotItems/HotItems';
-import { HotItemMocks, ItemMocks } from '@/mocks/PopUpDetailItemMocks';
-import { ItemPathType } from '@/types/DetailScreen';
 
 export default function PopUpDetailScreen() {
   const data = PopUpDetailMock;
-  const router = useRouter();
+  const renderHotItemWithIndex = useCallback(
+    (item: PopUpDetailItem, index: number) => (
+      <S.HotCardContainer key={item.id} isFirst={index === 0}>
+        <S.HotItemImage source={{ uri: item.image }} style={item.image} />
+        <S.Overlay />
+        <S.HotItemTitle numberOfLines={1}>{item.title}</S.HotItemTitle>
+        <S.HotItemPrice>{item.price}</S.HotItemPrice>
+      </S.HotCardContainer>
+    ),
+    [],
+  );
+
+  const renderItem = useCallback(
+    (item: PopUpDetailItem) => (
+      <S.ItemCard key={item.id}>
+        <S.ItemImage source={{ uri: item.image }} style={item.image} />
+        <S.ItemTitle numberOfLines={1}>{item.title}</S.ItemTitle>
+        <S.ItemPrice>{item.price}</S.ItemPrice>
+      </S.ItemCard>
+    ),
+    [],
+  );
 
   return (
-    <S.Container>
-      <S.Banner source={require('@/assets/images/common/popupimg.png')} />
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
+      <ScrollView>
+        <S.Banner source={require('@/assets/images/common/popupimg.png')} />
 
-      <S.PopUpContentBox>
-        <S.PopupTitle>{data.popupName}</S.PopupTitle>
-        <S.SubInfoRow>
-          <S.Icon source={require('@/assets/images/common/location-gray.webp')} />
-          <S.PopupInfo>{`${data.popupOpenDate} - ${data.popupCloseDate}`}</S.PopupInfo>
-        </S.SubInfoRow>
-        <S.SubInfoRow>
-          <S.Icon source={require('@/assets/images/common/calendar-gray.webp')} />
-          <S.PopupInfo>{data.address}</S.PopupInfo>
-        </S.SubInfoRow>
-      </S.PopUpContentBox>
+        <S.PopUpContentBox>
+          <S.PopupTitle>{data.popupName}</S.PopupTitle>
+          <S.SubInfoRow>
+            <S.Icon source={require('@/assets/images/common/location-gray.webp')} />
+            <S.PopupInfo>{`${data.popupOpenDate} - ${data.popupCloseDate}`}</S.PopupInfo>
+          </S.SubInfoRow>
+          <S.SubInfoRow>
+            <S.Icon source={require('@/assets/images/common/calendar-gray.webp')} />
+            <S.PopupInfo>{data.address}</S.PopupInfo>
+          </S.SubInfoRow>
+        </S.PopUpContentBox>
 
-      <S.Divider />
+        <S.Divider />
 
-      <S.PopUpContentBox>
-        <S.SectionTitle style={{ marginBottom: 8 }}>운영시간</S.SectionTitle>
-        <S.SubInfoRow>
-          <S.Icon source={require('@/assets/images/common/clock-gray.webp')} />
-          <S.PopupInfo>{`${data.runOpenTime.slice(0, 5)} - ${data.runCloseTime.slice(0, 5)}`}</S.PopupInfo>
-        </S.SubInfoRow>
+        <S.PopUpContentBox>
+          <S.SectionTitle style={{ marginBottom: 8 }}>운영시간</S.SectionTitle>
+          <S.SubInfoRow>
+            <S.Icon source={require('@/assets/images/common/clock-gray.webp')} />
+            <S.PopupInfo>{`${data.runOpenTime.slice(0, 5)} - ${data.runCloseTime.slice(0, 5)}`}</S.PopupInfo>
+          </S.SubInfoRow>
 
-        <S.SectionTitle style={{ marginTop: 20, marginBottom: 12 }}>위치정보</S.SectionTitle>
-        <S.MapImage source={{ uri: data.imageUrl }} />
-      </S.PopUpContentBox>
+          <S.SectionTitle style={{ marginTop: 20, marginBottom: 12 }}>위치정보</S.SectionTitle>
+          <S.MapImage source={{ uri: /* 나중에 지도로 변경 */ data.imageUrl }} />
+        </S.PopUpContentBox>
 
-      <S.DividerWide />
+        <S.DividerWide />
 
-      <S.ItemContentBox>
-        <S.ItemCategory style={{ marginTop: 40, marginBottom: 20 }}>WHAT’S HOT</S.ItemCategory>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          {HotItemMocks.map((item, index) => (
-            <HotItems key={index} item={item} index={index} />
-          ))}
-        </ScrollView>
-      </S.ItemContentBox>
+        <S.ItemContentBox>
+          <S.ItemCategory style={{ marginTop: 40, marginBottom: 20 }}>WHAT’S HOT</S.ItemCategory>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {MockItems.slice(0, 4).map((item, index) => renderHotItemWithIndex(item, index))}
+          </ScrollView>
+        </S.ItemContentBox>
 
-      <S.ItemContentBox>
-        <S.RowBetween>
-          <S.ItemCategoryAll>전체 상품</S.ItemCategoryAll>
-          <TouchableOpacity
-            onPress={() =>
-              router.push({
-                pathname: '/(common)/popUpDetail/entireItems',
-                params: {
-                  hotItems: ParseJsonToString(ItemMocks),
-                  title: data.popupName,
-                },
-              })
-            }
-          >
+        <S.ItemContentBox>
+          <S.RowBetween>
+            <S.ItemCategoryAll>전체 상품</S.ItemCategoryAll>
             <S.RightArrow source={require('@/assets/images/common/right-arrow.webp')} />
-          </TouchableOpacity>
-        </S.RowBetween>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ paddingRight: 24 }}
-        >
-          {ItemMocks.slice(0, 4).map((item: ItemPathType, idx: number) => (
-            <S.ItemCard key={idx}>
-              <S.ItemImage source={{ uri: item.imagePath }} />
-              <S.ItemTitle numberOfLines={1}>{item.title}</S.ItemTitle>
-              <S.ItemPrice>{item.price}</S.ItemPrice>
-            </S.ItemCard>
-          ))}
-        </ScrollView>
+          </S.RowBetween>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingRight: 24 }}
+          >
+            {MockItems.slice(0, 4).map(renderItem)}
+          </ScrollView>
 
-        <S.BottomButtonWrapper>
-          <CustomGradientBtn
-            title="팝업 예약하기"
-            height={54}
-            onPress={() => {
-              // TODO: 라우팅 구현 후 아래 줄 주석 해제
-              // navigation.navigate('PopupReservation');
-            }}
-            icon={require('@/assets/images/common/store-gray.webp')}
-          />
-        </S.BottomButtonWrapper>
-      </S.ItemContentBox>
-    </S.Container>
+          <S.BottomButtonWrapper>
+            <CustomGradientBtn
+              title="팝업 예약하기"
+              height={54}
+              onPress={() => {
+                // TODO: 라우팅 구현 후 아래 줄 주석 해제
+                // navigation.navigate('PopupReservation');
+              }}
+              icon={require('@/assets/images/common/store-gray.webp')}
+            />
+          </S.BottomButtonWrapper>
+        </S.ItemContentBox>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
