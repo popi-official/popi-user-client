35b76f2363c5aa743928c7a86996a087a8cf43be
yeongseoRyou <95837534+Y0ungse@users.noreply.github.com>
2025-05-28 14:13:25 +0900

    [LCR-242] feat: Modal UI구현

--- app/(common)/popUpEntry/index.tsx
+++ app/(common)/popUpEntry/index.tsx
@@ -2,6 +2,8 @@ import { Image, View } from 'react-native';
 import { S } from './PopUpEntry.style';
 import { popularItemList, recommendedItemList, reservationDetail } from '@/mocks/PopUpEntryMocks';
 import { useLocalSearchParams } from 'expo-router';
+import { useState } from 'react';
+import NoticeModal from '@/components/noticeModal/NoticeModal';
 
 const Images = {
   cameraBoy: require('@/assets/images/popUpEntry/camera-boy.webp'),
@@ -14,92 +16,121 @@ const Images = {
 
 const PopUpEntryScreen = () => {
   const { isSurvey } = useLocalSearchParams();
+  const [modalVisible, setModalVisible] = useState(false);
+
+  // 모달 열기
+  const handleCancelPress = () => {
+    setModalVisible(true);
+  };
+
+  // 실제 예약 취소 로직 + 모달 닫기
+  //  TODO: 예약 취소 API 호출
+  const handleConfirmCancel = () => {
+    setModalVisible(false);
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
 
   return (
-    <S.Container>
-      <S.PopUpEntryScreenContainer showsVerticalScrollIndicator={false}>
-        <S.Card>
-          {/* TopCard */}
-          <S.TopCard>
-            <S.Title>입장 QR</S.Title>
-            <S.CameraBoy source={Images.cameraBoy} style={{ width: 76, height: 76 }} />
-            <S.PopUpTitleContainer>
-              <Image source={Images.store} style={{ width: 18, height: 18, marginTop: 2 }} />
-              <S.PopupTitle>{reservationDetail.popupName}</S.PopupTitle>
-            </S.PopUpTitleContainer>
-            <S.InfoTextContainer>
-              <Image source={Images.locationDarkGray} style={{ width: 15, height: 15 }} />
-              <S.InfoText>{reservationDetail.address}</S.InfoText>
-            </S.InfoTextContainer>
-            <S.InfoTextContainer>
-              <Image source={Images.clockDarkGray} style={{ width: 15, height: 15 }} />
-              <S.InfoText>
-                {reservationDetail.popupDate} {reservationDetail.popupDay}{' '}
-                {reservationDetail.popupTime}
-              </S.InfoText>
-            </S.InfoTextContainer>
-          </S.TopCard>
+    <>
+      <S.Container>
+        <S.PopUpEntryScreenContainer showsVerticalScrollIndicator={false}>
+          <S.Card>
+            {/* TopCard */}
+            <S.TopCard>
+              <S.Title>입장 QR</S.Title>
+              <S.CameraBoy source={Images.cameraBoy} style={{ width: 76, height: 76 }} />
+              <S.PopUpTitleContainer>
+                <Image source={Images.store} style={{ width: 18, height: 18, marginTop: 2 }} />
+                <S.PopupTitle>{reservationDetail.popupName}</S.PopupTitle>
+              </S.PopUpTitleContainer>
+              <S.InfoTextContainer>
+                <Image source={Images.locationDarkGray} style={{ width: 15, height: 15 }} />
+                <S.InfoText>{reservationDetail.address}</S.InfoText>
+              </S.InfoTextContainer>
+              <S.InfoTextContainer>
+                <Image source={Images.clockDarkGray} style={{ width: 15, height: 15 }} />
+                <S.InfoText>
+                  {reservationDetail.popupDate} {reservationDetail.popupDay}{' '}
+                  {reservationDetail.popupTime}
+                </S.InfoText>
+              </S.InfoTextContainer>
+            </S.TopCard>
 
-          {/* QrCard */}
-          <S.QrCard>
-            <S.Description>이용하려는 팝업에 QR로 체크인하세요</S.Description>
-            <S.QRImage
-              source={{ uri: `data:image/png;base64,${reservationDetail.qrCodeBase64}` }}
-              resizeMode="contain"
-            />
-            <S.ButtonRow>
-              <S.Button isCancel>
-                <S.ButtonText isCancel>예약 취소</S.ButtonText>
-              </S.Button>
-              <S.Button>
-                <S.ButtonText>확인</S.ButtonText>
-              </S.Button>
-            </S.ButtonRow>
-          </S.QrCard>
-        </S.Card>
+            {/* QrCard */}
+            <S.QrCard>
+              <S.Description>이용하려는 팝업에 QR로 체크인하세요</S.Description>
+              <S.QRImage
+                source={{ uri: `data:image/png;base64,${reservationDetail.qrCodeBase64}` }}
+                resizeMode="contain"
+              />
+              <S.ButtonRow>
+                <S.Button isCancel onPress={handleCancelPress}>
+                  <S.ButtonText isCancel>예약 취소</S.ButtonText>
+                </S.Button>
+                <S.Button>
+                  <S.ButtonText>확인</S.ButtonText>
+                </S.Button>
+              </S.ButtonRow>
+            </S.QrCard>
+          </S.Card>
 
-        {isSurvey === '1' && (
-          <View>
-            {/* 취향저격 */}
-            <S.SectionTitle>몽몽님의 취향 저격</S.SectionTitle>
-            <S.SectionDescription>
-              예약하실 때 작성하셨던 설문지 내용을 기반으로 추천드려요
-            </S.SectionDescription>
-            <S.GoodsContainer>
-              {recommendedItemList.map(item => (
-                <S.GoodsItem key={item.itemId}>
-                  <S.GoodsImage source={item.imagePath} />
-                  <S.GoodsName numberOfLines={1} ellipsizeMode="tail">
-                    {item.title}
-                  </S.GoodsName>
-                  <S.GoodsPrice numberOfLines={1} ellipsizeMode="tail">
-                    {item.price.toLocaleString()}원
-                  </S.GoodsPrice>
-                </S.GoodsItem>
-              ))}
-            </S.GoodsContainer>
-          </View>
-        )}
+          {isSurvey === '1' && (
+            <View>
+              {/* 취향저격 */}
+              <S.SectionTitle>몽몽님의 취향 저격</S.SectionTitle>
+              <S.SectionDescription>
+                예약하실 때 작성하셨던 설문지 내용을 기반으로 추천드려요
+              </S.SectionDescription>
+              <S.GoodsContainer>
+                {recommendedItemList.map(item => (
+                  <S.GoodsItem key={item.itemId}>
+                    <S.GoodsImage source={item.imagePath} />
+                    <S.GoodsName numberOfLines={1} ellipsizeMode="tail">
+                      {item.title}
+                    </S.GoodsName>
+                    <S.GoodsPrice numberOfLines={1} ellipsizeMode="tail">
+                      {item.price.toLocaleString()}원
+                    </S.GoodsPrice>
+                  </S.GoodsItem>
+                ))}
+              </S.GoodsContainer>
+            </View>
+          )}
 
-        {/* 인기 상품 TOP 3 */}
-        <S.SectionTitle>인기 상품 TOP 3</S.SectionTitle>
-        <S.SectionDescription>매장 방문 전에 인기있는 상품을 확인해보세요</S.SectionDescription>
-        <S.GoodsContainer>
-          {popularItemList.map(item => (
-            <S.GoodsItem key={item.itemId}>
-              <S.GoodsImage source={Images.item} />
-              <S.GoodsName numbernumberOfLines={1} ellipsizeMode="tail">
-                {item.title}
-              </S.GoodsName>
-              <S.GoodsPrice numberOfLines={1} ellipsizeMode="tail">
-                {item.price.toLocaleString()}원
-              </S.GoodsPrice>
-            </S.GoodsItem>
-          ))}
-        </S.GoodsContainer>
-        <S.BottomArea />
-      </S.PopUpEntryScreenContainer>
-    </S.Container>
+          {/* 인기 상품 TOP 3 */}
+          <S.SectionTitle>인기 상품 TOP 3</S.SectionTitle>
+          <S.SectionDescription>매장 방문 전에 인기있는 상품을 확인해보세요</S.SectionDescription>
+          <S.GoodsContainer>
+            {popularItemList.map(item => (
+              <S.GoodsItem key={item.itemId}>
+                <S.GoodsImage source={Images.item} />
+                <S.GoodsName numbernumberOfLines={1} ellipsizeMode="tail">
+                  {item.title}
+                </S.GoodsName>
+                <S.GoodsPrice numberOfLines={1} ellipsizeMode="tail">
+                  {item.price.toLocaleString()}원
+                </S.GoodsPrice>
+              </S.GoodsItem>
+            ))}
+          </S.GoodsContainer>
+          <S.BottomArea />
+        </S.PopUpEntryScreenContainer>
+      </S.Container>
+
+      {/* 예약 취소 확인 모달 */}
+      <NoticeModal
+        visible={modalVisible}
+        title="예약을 취소하시겠습니까?"
+        onClose={handleCloseModal}
+        buttons={[
+          { title: '돌아가기', onPress: handleCloseModal },
+          { title: '예약 취소', onPress: handleConfirmCancel },
+        ]}
+      />
+    </>
   );
 };
 
