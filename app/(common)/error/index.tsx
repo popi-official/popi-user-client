22d601d5123bf22de794a73514f1e498f7a45a70
yeongseoRyou <95837534+Y0ungse@users.noreply.github.com>
2025-05-28 15:10:57 +0900

    [LCR-249] fix: lint 오류 수정

--- app/(common)/error/index.tsx
+++ app/(common)/error/index.tsx
@@ -1,31 +1,28 @@
 import { S } from './ErrorScreen.style';
 import { Image } from 'react-native';
-import { useRouter } from 'expo-router';
 
 export default function ErrorScreen() {
-    const router = useRouter();
+  return (
+    <S.Container>
+      <S.TextContainer>
+        <S.Title>예기치 못한 문제가 발생했어요</S.Title>
+        <S.Subtitle>홈으로 이동한 후, 다시 시도해주세요</S.Subtitle>
+      </S.TextContainer>
 
-    return(
-        <S.Container>
-        <S.TextContainer>
-          <S.Title>예기치 못한 문제가 발생했어요</S.Title>
-          <S.Subtitle>홈으로 이동한 후, 다시 시도해주세요</S.Subtitle>
-        </S.TextContainer>
-    
-        <S.ImageWrapper>
-          <Image
-            source={require('@/assets/images/error/error.webp')}
-            style={{ width: 256, height: 191 }}
-            resizeMode="contain"
-          />
-        </S.ImageWrapper>
-    
-        <S.GradientButton
-          title="HOME"
-          //onPress={() => router.push()}
-          fontSize={15}
-          fontWeight="700"
+      <S.ImageWrapper>
+        <Image
+          source={require('@/assets/images/error/error.webp')}
+          style={{ width: 256, height: 191 }}
+          resizeMode="contain"
         />
-      </S.Container>
-    );
+      </S.ImageWrapper>
+
+      <S.GradientButton
+        title="HOME"
+        //onPress={() => router.push()}
+        fontSize={15}
+        fontWeight="700"
+      />
+    </S.Container>
+  );
 }
