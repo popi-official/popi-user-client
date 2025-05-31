f569744069754050c93a3f103447e07f15682a4f
letthem <suyeon3662@naver.com>
2025-05-31 14:22:37 +0900

    [LCR-284] feat: 설문지 답변 등록 API 구현 & onSuccess 와 onError 모달 처리

--- src/hooks/api/useSurveyApi.ts
+++ src/hooks/api/useSurveyApi.ts
@@ -1,6 +1,6 @@
-import { getSurveyQuestions } from '@/apis/survey/SurveyApi';
-import { GetSurveyQuestionsRequest } from '@/types/api/ApiRequestType';
-import { useQuery } from '@tanstack/react-query';
+import { getSurveyQuestions, postSurveyAnswers } from '@/apis/survey/SurveyApi';
+import { GetSurveyQuestionsRequest, PostSurveyAnswersRequest } from '@/types/api/ApiRequestType';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export const useSurveyApi = ({ popupId }: GetSurveyQuestionsRequest) => {
   const query = useQuery({
@@ -13,4 +13,30 @@ export const useSurveyApi = ({ popupId }: GetSurveyQuestionsRequest) => {
     isLoading: query.isLoading,
     isError: query.isError,
   };
+};
+
+export const useSurveyAnswersApi = ({
+  onSuccess,
+  onError,
+}: {
+  onSuccess: () => void;
+  onError: (message: string) => void;
+}) => {
+  const queryClient = useQueryClient();
+
+  const postSurveyAnswersMutation = useMutation({
+    mutationFn: ({ popupId, answers }: { popupId: number; answers: PostSurveyAnswersRequest }) =>
+      postSurveyAnswers(popupId, answers),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['surveyAnswer'] });
+      onSuccess();
+    },
+
+    onError: () => {
+      onError('설문 제출에 실패했어요. 다시 시도해주세요.');
+    },
+  });
+
+  return { postSurveyAnswersMutation };
 };
