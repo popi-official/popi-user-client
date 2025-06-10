10f5d4972f116954829375233d4df4b5e79ad438
sung-yeop <jjabc9459@naver.com>
2025-06-10 11:15:57 +0900

    [LCR-330] chore: ESLint IgnorePattern 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  ignores: ['expo-env.d.ts'],
+  ignorePatterns: ['expo-env.d.ts', 'node_modules/', 'dist/', '*.config.js'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
