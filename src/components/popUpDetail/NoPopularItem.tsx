bc02f0fa08748597de4ebf5386a818de275b12f2
sung-yeop <jjabc9459@naver.com>
2025-06-10 10:04:14 +0900

    [LCR-328] fix: Lint 해결

--- src/components/popUpDetail/NoPopularItem.tsx
+++ src/components/popUpDetail/NoPopularItem.tsx
@@ -8,11 +8,7 @@ const Images = {
 export default function NoPopularItem() {
   return (
     <S.EmptyContainer>
-      <Image
-        source={Images.chart}
-        resizeMode="contain"
-        style={{ width: 100, tintColor: 'white', tintColor: 'gray' }}
-      />
+      <Image source={Images.chart} resizeMode="contain" style={{ width: 100, tintColor: 'gray' }} />
       <S.EmptyTitle>아직 인기상품 데이터가 없어요</S.EmptyTitle>
       <S.EmptyContent>지금은 모든 상품이 똑같이 특별해요!</S.EmptyContent>
     </S.EmptyContainer>
